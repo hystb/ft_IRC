c0bb6f61f406fb5ecd3fc0f934154fbac0eeb114
mdesmartin <mdesmart@student.42lyon.fr>
2023-11-06 12:51:52 +0100

    FIX: channel separated in cpp and hpp

--- src/messages/Channel.hpp
+++ src/messages/Channel.hpp
@@ -1,44 +1,30 @@
+#ifndef CHANNEL_HPP
+#define CHANNEL_HPP
+
 //#include <vector>
 #include <map>
 #include <iostream>
 #include "Client.hpp"
 
-class Channel {
-
+class Channel
+{
 	public:
 		Channel(const std::string& name) : _name(name) {}
-		//need canonical form
+		~Channel(void);
+		Channel& operator=(const Channel &parent);
+		Channel(const Channel &parent);
 
-		//map
-		// void addClient(Client &client) {
-		// 	_clients[client.getUsername()] = client.getIsModerator();
-		// }
-
-		void removeClient(const std::string& username) {
-			for (std::map<Client, bool>::iterator it = _clients.begin(); it != _clients.end(); ) {
-				if (it->first.getUsername() == username) {
-					_clients.erase(it++);
-				} else {
-					++it;
-				}
-			}
-		}
-		
-		void listClients() {
-			for (std::map<Client, bool>::const_iterator it = _clients.begin(); it != _clients.end(); ++it) {
-				const Client& client = it->first;
-				bool isModerator = it->second;
-				std::cout << "Client: " << client.getUsername();
-				if (isModerator)
-					std::cout << " Moderator" << std::endl;
-				else
-					std::cout << " Ordinary mortals" << std::endl;
-			}
-    	}
+		// map
+		void addClient(Client &client);
+		void removeClient(const std::string& username);
+		void listClients(void);
 
 		// setter???
 
 	private:
+		Channel(void);
 		std::string 			_name;
 		std::map<Client, bool>	_clients;//0 or 1 if moderator
 };
+
+#endif
